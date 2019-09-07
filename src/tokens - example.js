6a4bf41556e4b244de74ec78f0b9caad7bff95ac
hieudn1995 <53845464+hieudn1995@users.noreply.github.com>
2019-09-07 11:05:53 +0700

    Update tokens - example.js

--- src/tokens - example.js
+++ src/tokens - example.js
@@ -1,3 +1,3 @@
-module.exports.botToken = ''; //input your bot key
-module.exports.youtubeToken = ''; //input Youtube API key
-module.exports.devId = ''; //Used to lockdown, no purpose to use when using another bot
+module.exports.botToken = 'NjA4Mjk0NDA5MDg3NDgzOTE0.XXMp1A.AOSkuczo8uNzXqtOLp0oy9inUHU'; //input your bot key
+module.exports.youtubeToken = 'AIzaSyDFNoGoG5WcqxcKt6DtC8cDz-8gCQy7eLE'; //input Youtube API key
+module.exports.devId = '608294409087483914'; //Used to lockdown, no purpose to use when using another bot
